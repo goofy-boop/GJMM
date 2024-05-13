0fb2e16b689547ec76abc86d0c9fee333338b2f1
goofy-boop <131792145+goofy-boop@users.noreply.github.com>
2024-05-13 19:06:39 +0200

    Arbres + change de POV
    j'ai pris des arbres de l'asset store et j'ai programmé le changement du POV du player !!!

--- Assets/Regarder.cs
+++ Assets/Regarder.cs
@@ -9,6 +9,15 @@ public class Regarder : MonoBehaviour
 
     float xRotation = 0f;
 
+    bool povMonstre;
+
+    public Transform monstre;
+
+    public Transform playerCamPos;
+
+    public float distanceMonstre = 5f;
+
+    public LayerMask maskPlayer;
 
     private void Start()
     {
@@ -18,13 +27,24 @@ public class Regarder : MonoBehaviour
 
     private void Update()
     {
+        povMonstre = Physics.CheckSphere(monstre.position, distanceMonstre, maskPlayer);
+
         float x = Input.GetAxis("Mouse X") * vitesseDeCamera * Time.deltaTime;
         float y = Input.GetAxis("Mouse Y") * vitesseDeCamera * Time.deltaTime;
 
         xRotation -= y;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-        player.Rotate(Vector3.up * x);
+        if (!povMonstre)
+        {
+            transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+            player.Rotate(Vector3.up * x);
+            transform.position = playerCamPos.position;
+        }
+        else
+        {
+            transform.position = monstre.position;
+            transform.LookAt(player.position);
+        }
     }
 }
